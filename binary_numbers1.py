6489829114fe8b8953db013ab8b43bea4fba5630
Tommy Pujol <tommy@pujol.cf>
2019-03-09 09:12:21 +0000

    Create binary_numbers1.py
    Add a more simple numbers -> binary with the python's built in functions.

---
+++ binary_numbers1.py
@@ -0,0 +1,9 @@
+"""Copyright 2019 Tommy Pujol <tommy@pujol.cf>
+Detailed license : https://tommy.mit-license.org/ 
+GitHub Username : TommyPujol06
+
+A simple version to convert numbers to binary with the 
+python's built in function.
+"""
+num = int(input("Number : "))
+print(bin(num))
